6e39ac255a815ea57392558b4d3acbc70759dd0d
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-02-04 15:49:39 -0300

    refactor: Pydantic 2 migration - TypedDict to models, ConfigDict standards

--- src/flext_tap_oracle_wms/__init__.py
+++ src/flext_tap_oracle_wms/__init__.py
@@ -6,8 +6,6 @@ SPDX-License-Identifier: MIT.
 
 from __future__ import annotations
 
-from typing import Final
-
 from flext_core import FlextLogger, FlextModels, FlextResult
 from flext_meltano import FlextMeltanoBridge, FlextMeltanoService, FlextMeltanoSettings
 
@@ -29,16 +27,14 @@ from flext_tap_oracle_wms.exceptions import (
 from flext_tap_oracle_wms.models import FlextTapOracleWmsModels, m, m_tap_oracle_wms
 from flext_tap_oracle_wms.protocols import FlextTapOracleWmsProtocols
 from flext_tap_oracle_wms.streams import FlextTapOracleWmsStream
-from flext_tap_oracle_wms.utilities import FlextTapOracleWmsUtilities
-from flext_tap_oracle_wms.version import VERSION, FlextTapOracleWmsVersion
-
-# Domain-specific aliases
-u = FlextTapOracleWmsUtilities  # Utilities (FlextTapOracleWmsUtilities extends FlextUtilities)
-
-PROJECT_VERSION: Final[FlextTapOracleWmsVersion] = VERSION
-
-__version__: str = VERSION.version
-__version_info__: tuple[int | str, ...] = VERSION.version_info
+from flext_tap_oracle_wms.utilities import FlextTapOracleWmsUtilities as u
+from flext_tap_oracle_wms.version import (
+    VERSION,
+    VERSION as PROJECT_VERSION,
+    FlextTapOracleWmsVersion,
+    __version__,
+    __version_info__,
+)
 
 __all__ = [
     "PROJECT_VERSION",
